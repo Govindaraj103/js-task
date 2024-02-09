4381de549d11c60f1a1f9497ca000d39f1aad757
Govindaraj <118993139+Govindaraj103@users.noreply.github.com>
2024-02-09 07:45:47 -0800

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,90 @@
+//1.
+// let number=20;
+// let string="Govind";
+// let bool=true;
+
+// console.log(typeof number);
+// console.log(typeof string);
+// console.log(typeof bool);
+
+//2.
+//  function sum(a,b){
+//     return a+b;
+//  }
+//  console.log(sum(10,12));
+
+//3.
+
+// let i=10;
+
+//     if(i%2==0){
+//         console.log(`Evan Number --> ${i}`);
+//     }else{
+//         console.log(`Odd Number --> ${i}`);
+//     }
+
+
+//4.
+
+// let i=1;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+//5.
+
+// function num(number){
+// if(number>0){
+//     console.log(`${number} is Positive`);
+// }else if(number<0){
+//      console.log(`${number} is nagative`);
+
+// }else{
+//     console.log(`${number} is Zero`);
+// }
+// }
+// num(0);
+
+
+
+//6.
+
+// let n=5;
+// let string="";
+// for(let i=0;i<=n;i++){
+    
+//     for(let j=5-i;j<=n;j++){
+//         string+="*";
+//     }
+//     string+="\n"
+// }
+// console.log(string);
+
+//6.
+
+// let n=6;
+// let num=(1);
+
+// for(let i=1;i<=n;i++){
+//     num=num*i;
+// }
+// console.log(num);
+
+//7.
+
+
+
+
+
+
+// let array=[3,87,23,98,22,7,4]
+// let k=10;
+
+
+// function sum(array,k){
+
+    
+
+// }
+// sum(array,k)
